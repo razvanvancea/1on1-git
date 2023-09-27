8040df1548c6d792513bc7a18a5e99a3f8d428cd
Razvan Vancea <iamqarv@gmail.com>
2023-09-27 07:46:11 +0300

    update

--- test.js
+++
@@ -1,9 +0,0 @@
-console.log('Welcome to Git lesson');
-qweqwewe
-lorem ipsum
-
-
-sdsd
-
-new changes by ale
-text 2
---
+++ tests/test.js
@@ -0,0 +1,10 @@
+import page from '../pages/page-model';
+import { Selector } from 'testcafe';
+fixture `Test suite example`
+    .page `https://razvanvancea.ro`;
+
+test('Test workshops section', async t => {
+    await t
+        .click(Selector('#nav-menu a[href="#services"]'))
+        .expect(Selector('h3.services__title').withText('1 on 1').exists).ok(); 
+});
